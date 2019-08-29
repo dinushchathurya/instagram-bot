4b62cfc90d5ab881d1f55190b89c290971880afc
dinushchathurya <dinushchathurya21@gmail.com>
2019-08-29 22:22:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,14 @@ from selenium.webdriver.common.keys import keys
 import time
 
 class InstgramBot():
+  def __init__ (self,username, password)
+     self.username = username
+     self.password = password
+     self.driver = webdriver.Firefox()
+
+  def closebrowser(self):
+    self.driver.close()
+
+  def login(self)
+     
 
