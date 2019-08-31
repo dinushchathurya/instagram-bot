be27f96a5eff10edb13e1ed8ca1dc93274b84888
dinushchathurya <dinushchathurya21@gmail.com>
2019-08-31 22:06:32 +0530

    Fixed error in login function

--- app.py
+++ app.py
@@ -15,10 +15,18 @@ class InstagramBot():
     driver = self.driver
     driver.get("https://www.instagram.com/")
     time.sleep(2)
-    login_button =driver.find_element_by_xpath("//a[a@href'accounts/login']")
+    login_button =driver.find_element_by_xpath("//a[a@href='/accounts/login/']")
     login_button.click()
     time.sleep(2)
-    
+    user_name_elem = driver.find_element_by_xpath ("//input[@name='username']")
+    user_name_elem.clear()
+    user_name_elem.send_keys(self.username)
+    password_elem =driver.find_element_by_xpath ("//input[@name='password']")
+    password_elem.clear()
+    password_elem.send_keys(self.password)
+    password_elem.send_keys(keys.RETURN)
+    time.sleep(2)
+
 
 andreyIG = InstagramBot("username","password")
 andreyIG.login()
