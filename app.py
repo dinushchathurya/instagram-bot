6449a39d757c21fddd0875d84053b85f369c13f1
dinushchathurya <dinushchathurya21@gmail.com>
2019-08-31 21:49:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,22 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import keys
+from selenium.webdriver.common import keys
 import time
 
-class InstgramBot():
-  def __init__ (self,username, password)
+class InstagramBot():
+  def __init__ (self,username, password):
      self.username = username
      self.password = password
-     self.driver = webdriver.Firefox()
+     self.driver = webdriver.Chrome()
 
   def closebrowser(self):
     self.driver.close()
 
-  def login(self)
+  def login(self):
+    driver = self.driver
+    driver.get("https://www.instagram.com/")
+    time.sleep(2)
+
+andreyIG = InstagramBot("username","password")
+andreyIG.login()
      
 
